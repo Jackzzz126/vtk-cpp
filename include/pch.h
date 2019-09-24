97b1962df3e58f20bc7033ee20a4fa94a9d8fa6c
Jackzzz126 <Jackzzz@126.com>
2019-09-24 10:10:08 +0800

    ref: code structure

---
+++ include/pch.h
@@ -0,0 +1,20 @@
+//std
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+//vtk
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkSmartPointer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkPolyData.h>
+#include <vtkSphereSource.h>
+#include <vtkWindowToImageFilter.h>
+#include <vtkPNGWriter.h>
+
